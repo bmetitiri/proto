5435bfd5dbbfa78994a455984ce1b7a295bd52a6
arkie <email@joncaldwell.com>
2010-04-14 02:02:31 -0700

    basic stylings

--- common.py
+++ common.py
@@ -8,14 +8,19 @@ SERIAL_URL = '\.?(?P<ext>html|json|xml)?$'
 
 	########## Regexes ##########
 import re
-_BOLD       = re.compile('\*(.+?)\*')
-_ITALIC     = re.compile('\_(.+?)\_')
-_STRIKE     = re.compile('\-(.+?)\-')
-_INSERT     = re.compile('\+(.+?)\+')
-
-def parse(s):
-	s = _BOLD.sub('<strong>\\1</strong>', s)
-	s = _ITALIC.sub('<em>\\1</em>',       s)
-	s = _STRIKE.sub('<del>\\1</del>',     s)
-	s = _INSERT.sub('<ins>\\1</ins>',     s)
-	return s
+_LINKS     = re.compile(r'([\w+]+://\S+[\w\d/])')
+_TAGS      = re.compile(r'(?<![\S])([*_+-])(.+?)\1(?=[\s.!?]|$)')
+_PARAGRAPH = re.compile(r'(.+?)(\n\r?\n|$)', re.DOTALL)
+def _tags(match):
+	MATCHES = {
+		'*': ('<strong>', '</strong>'),
+		'_': ('<em>',     '</em>'),
+		'+': ('<ins>',    '</ins>'),
+		'-': ('<del>',    '</del>'),
+	}
+	tag = MATCHES[match.group(1)]
+	return '%s%s%s' % (tag[0], match.group(2), tag[1])
+def parse(target):
+	target = _LINKS.sub(r'<a href="\1">\1</a>', target)
+	target = _PARAGRAPH.sub(r'<p>\1</p>', target) #.replace('\n', '<br />')
+	return _TAGS.sub(_tags, target)
