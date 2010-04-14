cc65b0059bcc60243897f17112de75e57f581c46
arkie <email@joncaldwell.com>
2010-04-13 21:08:42 -0700

    args>a, kargs>kw, why not?

--- vertex.py
+++ vertex.py
@@ -6,10 +6,11 @@ import os.path, common
 class Vertex(db.Model):
 	name    = db.StringProperty()
 	content = db.TextProperty()
+	html    = db.TextProperty()
 	created = db.DateTimeProperty(auto_now_add=True)
-	def __init__(self, url, *args, **kargs):
-		kargs['key_name'] = url
-		super(Vertex, self).__init__(*args, **kargs)
+	def __init__(self, url, *a, **kw):
+		kw['key_name'] = url
+		super(Vertex, self).__init__(*a, **kw)
 		if not self.name:
 			if url == '/': self.name = '(index)'
 			else: self.name = url.replace('/', '').replace('-', ' ').title()
@@ -24,6 +25,8 @@ class Vertex(db.Model):
 	@property 
 	def edit_url(self):
 		return '/edit%s' % self.url
+	def put(self, *a, **kw):
+		super(Vertex, self).save(*a, **kw)
 	@property
 	def url(self):
 		return '%s'      % self.key().name()
@@ -52,7 +55,7 @@ class VertexEdit(BaseHandler):
 	def get (self, url):
 		vertex = Vertex.get_by_key_name(url) or Vertex(url)
 		self.render({'vertex': vertex})
-	def post(self, url, *args, **kargs):
+	def post(self, url, *a, **kw):
 		vertex         = Vertex.get_by_key_name(url) or Vertex(url)
 		vertex.name    = self.request.get('name')
 		vertex.content = self.request.get('content')
