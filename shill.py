da412c6b591a467c1bb97d1fa77f896d484b7ae9
arkie <email@joncaldwell.com>
2010-04-22 01:40:49 -0700

    interactive persistence

--- shill.py
+++ shill.py
@@ -1,28 +1,72 @@
 from google.appengine.ext.webapp import  WSGIApplication, template, util
 from google.appengine.ext import db, webapp
-import new, urllib, __builtin__
+from cStringIO import StringIO
+import cgi, pickle, sys, traceback, urllib, __builtin__
 
-class Shill(db.Model, webapp.RequestHandler):
+class Out:
+	def __init__(self): self._string = ''
+	def write(self, output):
+		self._string += output
+	def _ignore(self, *args): pass
+	def __str__(self):
+		string        = self._string
+		self._string  = ''
+		return string
+	seek = flush = readline = _ignore
+
+class History(db.Model):
+	input  = db.ListProperty(db.Text)
+	output = db.ListProperty(db.Text)
+	world  = db.BlobProperty()
+	@property
+	def html(self):
+		out = ''.join(['>>> %s\n%s' % (k, v)
+			for k,v in zip(self.input, self.output)])
+		return cgi.escape(out).replace('\n', '<br />')
+	def append(self, input, output):
+		self.input.append(db.Text(input))
+		self.output.append(db.Text(output))
+
+class Shill(webapp.RequestHandler):
 	def get(self, key):
-		key   = urllib.unquote(key)
-		self.render({'key':key})
+		key           = urllib.unquote(key)
+		self.render({'key':key, 'output':self.history.html})
 	def post(self, key):
-		world    = {'__builtins__':__builtin__, 'db':db}
-		module    = self.request.get('module').replace('\r\n', '\n')
-		input = self.request.get('input').replace('\r\n', '\n')
-		exec module in world
+		world         = {}
+#		world['__builtins__'] = __builtin__
+		module        = self.request.get('module').replace('\r\n', '\n')
+		input         = self.request.get('input').replace('\r\n', '\n')
+		oldout        = sys.stdout
+		sys.stdout    = Out()
+		try:
+			code      = compile(module, '(module code)', 'exec')
+			exec code in world
+		except: print traceback.format_exc()
+		modout        = str(sys.stdout)
 		if input.strip():
-			output   = eval(input, world)
-			if isinstance(output, unicode):
-				output = "u'%s'" % output
-			elif isinstance(output, basestring):
-				output = "'%s'" % output
-			else:
-				output = str(output)
-			output     = '&gt;&gt;&gt; ' + input + '<br />' + output
-		else:
-			output     = '&gt;&gt;&gt;'
+			try:
+				world.update(pickle.loads(str(self.history.world)))
+				code  = compile(input, '(interactive mode)', 'single')
+				exec code in world
+			except: print traceback.format_exc()
+			output    = str(sys.stdout)
+			self.history.append(input, output)
+			self.history.world = pickle.dumps(dict([(k, world[k]) for k in
+				world if k not in ['__builtins__', 'w']]))
+			self.history.put()
+		output        = self.history.html
+		sys.stdout    = oldout
+		if modout:
+			output    = '(module init)<br />' + modout + '<br />' + output
 		self.render({'key':key, 'module':module, 'output':output})
+
+	@property
+	def history(self):
+		if not hasattr(self, '_history'):
+			key       = self.request.query_string or '(index)'
+			self._history = History.get_by_key_name(
+					key) or History(key_name=key)
+		return self._history
 	def render(self, values=None):
 		if not values: values={}
 		self.response.headers['Content-Type'] = 'text/html'
@@ -30,7 +74,7 @@ class Shill(db.Model, webapp.RequestHandler):
 		self.response.out.write(out)
 
 application = WSGIApplication([
-		('/(?P<key>.*)', Shill),
+		('/(?P<key>.*)/?edit', Shill),
 	], debug=True)
 
 def main(): util.run_wsgi_app(application)
--- static/shill.js
+++ static/shill.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
+	anchor = document.location.toString().split('#')[1];
 	$(window).resize(function(){
 			$('#input').css({'font-size':'15px'}).height(20);
 			$('#module').height($(window).height()-52);
 			$('#output').height($(window).height()-74);
 		}).resize()
+	$('#output').scrollTop($('#output')[0].scrollHeight);
 	$('body').prepend('<ul id="menu" />')
 	$('fieldset').css({'margin':0, 'height':'100%','padding':0, 'width':'100%'}
 		).hide()
@@ -13,13 +15,15 @@ $(document).ready(function(){
 	$('label').hide(); $('legend').hide(); $('#update').hide();
 	$('legend').each(function(){$('#menu').append(
 		$('<li class="nav">'+$(this).text()+'</li>').data('fs', $(this)))})
-	$('.nav').css({'cursor':'pointer', 'float':'left', 'line-height':'50px',
-		'list-style-type':'none', 'text-align':'center', 'width':'50px',}
+	$('#menu').css({'margin':'auto'}).width($('#menu>li').length*50)
+	$('.nav').css({'background':'#fff', 'color':'#000', 'cursor':'default',
+		'float':'left', 'line-height':'50px', 'list-style-type':'none',
+		'text-align':'center', 'width':'50px',}
 		).hover(function(){
+			$('.nav').css({'background':'#fff', 'color':'#000'})
 			$('fieldset').hide();
 			$(this).data('fs').parent().show();
-			$(this).css({'background':'#fff', 'color':'#000'})},
-			function(){$(this).css({'background':'#000', 'color':'#fff'})}
-		).mouseleave()
-	$('#menu').css({'margin':'auto'}).width($('#menu>li').length*50)
+			$(this).css({'background':'#000', 'color':'#fff'});
+			$('input').focus(); $('textarea').focus();
+		})
 });
