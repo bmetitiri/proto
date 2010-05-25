a71e12d10885f53c1b1f692deca3f6e02b0d26ed
arkie <email@joncaldwell.com>
2010-05-24 20:10:39 -0700

    initial commit

---
+++ src/arkie/shing/Shing.java
@@ -0,0 +1,15 @@
+package arkie.shing;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Shing extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
