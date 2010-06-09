f690ca4907521bef2e2c71e65eb6eb6949af8d95
arkie <email@joncaldwell.com>
2010-06-08 20:47:09 -0700

    Removing xml definitions

--- src/arkie/shing/Shing.java
+++ src/arkie/shing/Shing.java
@@ -8,15 +8,12 @@ import android.widget.TextView;
 
 public class Shing extends Activity
 {
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.main);
-		Typeface uni = Typeface.createFromAsset(
-				getAssets(), "fonts/alterebro-pixel-font.ttf");
-		((TextView)findViewById(R.id.handle)).setTypeface(uni);
+//		Typeface uni = Typeface.createFromAsset(
+//				getAssets(), "fonts/alterebro-pixel-font.ttf");
 	}
 }
