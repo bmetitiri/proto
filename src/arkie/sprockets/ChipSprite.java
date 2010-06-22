898dc954a9ff3f70f22506d86d329780337ff1ab
arkie <email@joncaldwell.com>
2010-06-21 23:37:47 -0700

    remove some overloading, boo java

--- src/arkie/sprockets/ChipSprite.java
+++ src/arkie/sprockets/ChipSprite.java
@@ -6,11 +6,6 @@ import android.graphics.*; //Bitmap, Point
 public class ChipSprite extends Sprite {
 	int columns, rows;
 	Point fallback;
-	public ChipSprite(){}
-	public ChipSprite(Bitmap bitmap, int width, int height){
-		super(bitmap);
-		this.setSize(width, height);
-	}
 	public ChipSprite(Context context, int bitmap, int width, int height){
 		super(context, bitmap);
 		this.setSize(width, height);
@@ -22,9 +17,6 @@ public class ChipSprite extends Sprite {
 	}
 	public void setDefault(int x, int y){
 		this.fallback = new Point(x, y);
-	}
-	public void setDefault(Point fallback){
-		this.fallback = fallback;
 	}
 	public void draw(Canvas canvas, float xout, float yout){
 		int x = this.fallback.x*this.width;
