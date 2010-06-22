6291b19fc2c5aefa51f49853ecc3b3caf3ac6f69
arkie <email@joncaldwell.com>
2010-06-22 01:09:54 -0700

    drawing from chipset again

--- src/arkie/shing/Shing.java
+++ src/arkie/shing/Shing.java
@@ -10,15 +10,8 @@ public class Shing extends Activity {
 	@Override public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		ChipSprite c = new ChipSprite(this, R.drawable.chip, 16, 16);
-		c.setDefault(5,5);
-		Player p = new Player(this, R.drawable.poke);
-		Sprite[][] map = new Sprite[][]{
-			{c,c,c},
-			{c,c,c},
-			{c,c,c},
-		};
-		SpriteMap main = new SpriteMap(map, c);
-		setContentView(new SpriteView(this, main));
+		SprocketView view = new SprocketView(this, R.drawable.chip);
+		view.setFallback(5,5);
+		setContentView(view);
 	}
 }
