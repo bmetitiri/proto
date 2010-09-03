913f773375b4fd7d354a7e37e1d7ef6826e93187
arkie <email@joncaldwell.com>
2010-09-03 01:18:53 -0700

    live-loading is golden ... and the color are rainbow :P

--- example.js
+++ example.js
@@ -2,10 +2,13 @@
 
 var glomp = require('./glomp.js');
 
-template = {html:{body:[
-	{h1:{$style:"color:#0f0;",$:'Hello World!'}},
-	{h1:{$style:"color:#ff0;",$:'Hello World!'}},
-	{h1:{$style:"color:#0ff;",$:'Hello World!'}},
+template = {html:{head:{style:{$type:'text/css',
+	$:'body {background:#00f; text-align:center}'}},body:[
+	{h1:{$style:'color:#0f0;',$:'Hello World!'}},
+	{h1:{$style:'color:#ff0;',$:'Hello World!'}},
+	{h1:{$style:'color:#0ff;',$:'Hello World!'}},
+	{h1:{$style:'color:#f0f;',$:'Hello World!'}},
+	{h1:{$style:'color:#f00;',$:'Hello World!'}},
 	]}}
 
 glomp.urls({
