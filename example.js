47f411c65e92dc6911e449953f944c59eebdfcae
arkie <email@joncaldwell.com>
2010-09-03 01:33:01 -0700

    now just playing around <<

--- example.js
+++ example.js
@@ -1,20 +1,18 @@
 #!/usr/bin/node
 
-var glomp = require('./glomp.js');
-
-glomp.urls({
+require('./glomp.js').urls({
 	'/' : function(io){
 		var template = {html:{head:{style:{$type:'text/css',
 			$:'body {background:#00f; text-align:center}'}},
 			meta:{'$http-equiv':'refresh',$content:1},body:[
 			{h1:{$style:'color:#0f0;',$:'Hello World!'}},
-			{h1:{$style:'color:#ff0;',$:'Hello World!'}},
-			{h1:{$style:'color:#0ff;',$:'Hello World!'}},
-			{h1:{$style:'color:#f0f;',$:'Hello World!'}},
-			{h1:{$style:'color:#f00;',$:'Hello World!'}},
+			{h2:{$style:'color:#ff0;',$:'Hello World!'}},
+			{h3:{$style:'color:#0ff;',$:'Hello World!'}},
+			{h4:{$style:'color:#f0f;',$:'Hello World!'}},
+			{h5:{$style:'color:#f00;',$:'Hello World!'}},
 			]}}
 
-		template.html.body.push({h1:'What\'s up?'})
+		template.html.body.push({h6:'What\'s up?'})
 		io.template(template);
 	}
 }).debug();
