b31c693ad2d7bbc6ca1e3dc22653d015574f8d54
arkie <email@joncaldwell.com>
2010-09-03 01:31:28 -0700

    refresh header "example"

--- example.js
+++ example.js
@@ -2,17 +2,18 @@
 
 var glomp = require('./glomp.js');
 
-var template = {html:{head:{style:{$type:'text/css',
-	$:'body {background:#00f; text-align:center}'}},body:[
-	{h1:{$style:'color:#0f0;',$:'Hello World!'}},
-	{h1:{$style:'color:#ff0;',$:'Hello World!'}},
-	{h1:{$style:'color:#0ff;',$:'Hello World!'}},
-	{h1:{$style:'color:#f0f;',$:'Hello World!'}},
-	{h1:{$style:'color:#f00;',$:'Hello World!'}},
-	]}}
-
 glomp.urls({
 	'/' : function(io){
+		var template = {html:{head:{style:{$type:'text/css',
+			$:'body {background:#00f; text-align:center}'}},
+			meta:{'$http-equiv':'refresh',$content:1},body:[
+			{h1:{$style:'color:#0f0;',$:'Hello World!'}},
+			{h1:{$style:'color:#ff0;',$:'Hello World!'}},
+			{h1:{$style:'color:#0ff;',$:'Hello World!'}},
+			{h1:{$style:'color:#f0f;',$:'Hello World!'}},
+			{h1:{$style:'color:#f00;',$:'Hello World!'}},
+			]}}
+
 		template.html.body.push({h1:'What\'s up?'})
 		io.template(template);
 	}
