f3bb58e978087cce5d2c6ff515558ccabeb4a1d0
arkie <email@joncaldwell.com>
2010-09-04 10:26:03 -0700

    remove old dict-style template code from example

--- example.js
+++ example.js
@@ -2,18 +2,6 @@
 
 require('./glomp.js').templates(['index.html']).urls({
 	'/' : function(io){
-/*		var template = {html:{head:{style:{$type:'text/css',
-			$:'body {background:#00f; text-align:center}'}},
-			meta:{'$http-equiv':'refresh',$content:1},body:[
-			{h1:{$style:'color:#0f0;',$:'Hello World!'}},
-			{h2:{$style:'color:#ff0;',$:'Hello World!'}},
-			{h3:{$style:'color:#0ff;',$:'Hello World!'}},
-			{h4:{$style:'color:#f0f;',$:'Hello World!'}},
-			{h5:{$style:'color:#f00;',$:'Hello World!'}},
-			]}}
-
-		template.html.body.push({h6:'What\'s up?'})
-		io.render(template);*/
 		io.template('index.html');
 	}
 }).debug();
