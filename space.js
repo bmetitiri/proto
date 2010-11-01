2f6bcf1bf1b844cf89c9daf36d36a4694ce6951b
arkie <email@joncaldwell.com>
2010-10-31 17:43:19 -0700

    collisions

--- space.js
+++ space.js
@@ -1,12 +1,25 @@
-CONNECTION_ID  = '+';
 OBJECT_COUNT   = 1;
 ROTATION_SPEED = .005;
 THRUSTER_SPEED = .1;
+THRUSTER_COLOR = '#fa3';
 BULLET_SPEED   = 10;
-world          = {}
 
+KEYS = {65:'starboard', 68:'port', 87:'aft', 16:'fire',
+		37:'starboard', 39:'port', 38:'aft', 17:'fire'}
+
+world = {}
+
+function shape(){
+	ctx.beginPath();
+	ctx.fillStyle = arguments[0];
+	ctx.moveTo(arguments[1], arguments[2]);
+	for (var i = 3; i < arguments.length; i+=2)
+		ctx.lineTo(arguments[i], arguments[i+1]);
+	ctx.fill();
+}
 exports = {
-	bullet : function(id, data){//id, x, y, deltax, deltay, r
+	bullet : function(id, data){
+		this.collide = [];
 		this.id = id; this.x = data.x; this.y = data.y; this.r = data.r;
 		this.deltax = data.deltax; this.deltay = data.deltay;
 		this.deltax += Math.cos(this.r) * BULLET_SPEED;
@@ -15,7 +28,7 @@ exports = {
 			ctx.save();
 			ctx.translate(this.x, this.y);
 			ctx.rotate(this.r);
-			/* Blaster
+			/* Blaster graphic
 			ctx.fillStyle = '#3af'
 			ctx.fillRect(0, -1, 5, -2);
 			ctx.fillRect(0, 1, 5, 2); */
@@ -30,11 +43,14 @@ exports = {
 				delete world[id]
 			}
 		}
+		this.bounds = function(){
+			return {left:this.x-2, top:this.y-2,
+				right:this.x+2, bottom:this.y+2}
+		}
 	},
-
-	ship : function(id, data){//id, x, y
+	ship : function(id, data){
 		this.type      = 'ship';
-		// Thrusters
+		this.collide   = [];
 		this.port      = data.port      || false;
 		this.starboard = data.starboard || false;
 		this.aft       = data.aft       || false;
@@ -48,37 +64,11 @@ exports = {
 			ctx.save();
 			ctx.translate(this.x, this.y);
 			ctx.rotate(this.r);
-			ctx.beginPath();
-			ctx.fillStyle = '#000'
-			ctx.moveTo(10, 0);
-			ctx.lineTo(-10, 8);
-			ctx.lineTo(0, 0);
-			ctx.lineTo(-10, -8);
-			ctx.fill();
-			if (this.starboard){
-				ctx.beginPath();
-				ctx.fillStyle = '#fa3'
-				ctx.moveTo(8, 2);
-				ctx.lineTo(5, 7);
-				ctx.lineTo(11, 7);
-				ctx.fill();
-			}
-			if (this.port){
-				ctx.beginPath();
-				ctx.fillStyle = '#fa3'
-				ctx.moveTo(8, -2);
-				ctx.lineTo(5, -7);
-				ctx.lineTo(11, -7);
-				ctx.fill();
-			}
-			if (this.aft){
-				ctx.beginPath();
-				ctx.fillStyle = '#fa3'
-				ctx.moveTo(-4, 0);
-				ctx.lineTo(-8, -4);
-				ctx.lineTo(-8, 4);
-				ctx.fill();
-			}
+			var color = (this.collide.length)?'#f00':'#000';
+			shape(color, 10, 0, -10, 8, 0, 0, -10, -8);
+			if (this.starboard) shape(THRUSTER_COLOR, 8, 2, 5, 7, 11, 7);
+			if (this.port)      shape(THRUSTER_COLOR, 8, -2, 5, -7, 11, -7);
+			if (this.aft)       shape(THRUSTER_COLOR, -4, 0, -8, -4, -8, 4);
 			ctx.restore();
 		}
 		this.update = function(){
@@ -90,10 +80,6 @@ exports = {
 				this.deltay += Math.sin(this.r) * THRUSTER_SPEED;
 			}
 			if (this.fire){
-/*				if (PLAYER_ID = this.id)
-					send(CONNECTION_ID + OBJECT_COUNT++,
-						{x:this.x, y:this.y, deltax:this.deltax,
-						deltay:this.deltay, r:this.r, type:'bullet'});*/
 				shot = new exports.bullet('0.'+OBJECT_COUNT++,
 						{x:this.x, y:this.y, deltax:this.deltax,
 						deltay:this.deltay, r:this.r});
@@ -112,74 +98,69 @@ exports = {
 			if (this.x > cvs.width || this.x < 0)  this.deltax = -this.deltax;
 			if (this.y > cvs.height || this.y < 0) this.deltay = -this.deltay;
 		}
+		this.bounds = function(){
+			return {left:this.x-5, top:this.y-5,
+				right:this.x+5, bottom:this.y+5}
+		}
 	}
 }
 function main(){
 	ctx.clearRect(0, 0, cvs.width, cvs.height);
 	for (var o in world){
+		world[o].collide = [];
+		for (var oo in world) //TODO: O(n2) => O(nlogn)
+			if (o != oo){
+				var r1 = world[o].bounds()
+				var r2 = world[oo].bounds()
+				if (r1.top < r2.bottom && r2.top < r1.bottom &&
+						r1.left < r2.right && r2.left < r1.right)
+					world[o].collide.push(world[oo]);
+			}
 		world[o].update();
 		if (world[o]) world[o].draw();
 	}
 }
-function init(){
-	setInterval(main, 33);
-	setInterval(function(){
-		id = CONNECTION_ID+'wad+shift';
-		send(id, world[id]);
-	}, 1000);
-}
 function receive(data){
-//	var data = JSON.parse(data);
 	for (var k in data){
 		if (k in world){
 			if (data[k] == 'delete') delete world[k];
 			else for (var v in data[k]) world[k][v] = data[k][v];
-		} else if (data[k]['type'])
+		} else if (data[k]['type']){
 			world[k] = new exports[data[k]['type']](k, data[k]);
+		}
 	}
 }
 function send(id, data){
-	var env = {}; env[id] = data;
 	socket.send(data);
+	var env = {}; env[id] = data;
 	receive(env);
-//	receive(JSON.stringify(env));
 }
-keys = {'wad+shift':{65:'starboard', 68:'port', 87:'aft', 16:'fire'},
-		'arrows+ctrl':{37:'starboard', 39:'port', 38:'aft', 17:'fire'}}
-
 window.onload = function(){
 	socket = new io.Socket(null, {port: 8080});
 	socket.on('message', function(data){
 		receive(data);
-		console.log(data);
 	});
 	socket.connect();
 
 	cvs = document.getElementById('canvas');
 	cvs.width  = 400;
 	cvs.height = 400;
-	cvs.style.cssText = 'border:1px solid #000';
-//	cvs.width  = window.innerWidth;
-//	cvs.height = window.innerHeight;
 	ctx = cvs.getContext('2d');
 
-//	for (var type in keys){
-	type = 'wad+shift';
-		send(CONNECTION_ID+type, {type:'ship'});
-//	}
+	send('^', {type:'ship'});
 	window.onkeyup = window.onkeydown = function(e){
 		action = {}
-//		for (var type in keys){
-		type = 'wad+shift';
-			action = keys[type][e.keyCode];
-			if (action){
-				state = e.type == 'keydown';
-				if (world[CONNECTION_ID+type][action] != state){
-					env = {}; env[action] = state;
-					send(CONNECTION_ID+type, env);
-				}
+		action = KEYS[e.keyCode];
+		if (action){
+			state = e.type == 'keydown';
+			if (world['^'][action] != state){
+				env = {}; env[action] = state;
+				send('^', env);
 			}
-//		}
+		}
 	}
-	init();
+	setInterval(main, 33);
+	setInterval(function(){
+		send('^', world['^']);
+	}, 1000);
 }
