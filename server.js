445da93bd80acfaf2c8164da7cb91f09777d7ee1
arkie <email@joncaldwell.com>
2011-01-04 02:20:48 -0800

    cross code usage from server to frontend

--- server.js
+++ server.js
@@ -3,21 +3,60 @@ var http = require('http'),
 	io   = require('socket.io');
 	fs   = require('fs');
 
+var format_twitter = function(data){
+	var message = ['<div class="inner '];
+	if (data.media){
+		message = message.concat(
+			['container" style="background: url(',
+			data.media.thumb,') no-repeat center center']);
+	}
+	message = message.concat([
+		'"><canvas width="100" height="100"></canvas>',
+		'<a href="http://twitter.com/', data.screen, '">',
+		'<img class="profile" src="', data.image,'" />',
+		data.name, '[', data.followers, ']</a> ', data.text, '</div>']);
+	if (data.media)
+		if (data.media.type == 'image')
+			message = message.concat(['<a href="', data.media.link,
+					'"><img class="media" src="', data.media.source,
+					'" /></a>']);
+		else message = message.concat([
+				'<iframe class=" media youtube-player"',
+				'type="text/html" width="640" height="385"',
+				'src="http://www.youtube.com/embed/', data.media.source,
+				'" frameborder="0"></iframe>']);
+	return message.join('');
+}
+
 //var cwd   = process.argv[1];
 var cwd   = __filename.slice(0, __filename.lastIndexOf('/')+1);
-var index = fs.readFileSync(cwd+'index.html');
+var index = fs.readFileSync(cwd+'index.html').toString();
+index = index.replace('function(data){/*placeholder*/}',
+		format_twitter.toString());
 
 //fs.watchFile(cwd+'index.html', function(){
 //	fs.readFile(cwd+'index.html', function(e, data){index = data;})});
 
 var user = process.env.TWITTER_USERNAME,
-	pass = process.env.TWITTER_PASSWORD;
+	pass = process.env.TWITTER_PASSWORD,
+	log  = [];
+
 
 // Server
 
 var server = http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end(index);
+
+	var needle  = '<div id="content">';
+	var slice   = index.indexOf(needle)+needle.length;
+	res.write(index.slice(0, slice));
+
+	for (var data in log){
+		res.write('<div data-twitter="'+JSON.stringify(log[data]).replace(/"/g,
+			'`')+ '" class="message">'+format_twitter(log[data])+'</div>');
+	}
+
+	res.end(index.slice(slice));
 });
 server.listen(process.env.PORT||8080);
 var socket = io.listen(server);
@@ -26,7 +65,6 @@ var socket = io.listen(server);
 
 var auth    = 'Basic ' + new Buffer(user + ':' + pass).toString('base64'),
 	buf     = Buffer('');
-//	log     = [];
 
 var twitter = http.createClient(80, 'stream.twitter.com');
 var handle  = function(data){
@@ -84,9 +122,8 @@ var handle  = function(data){
 					url.url+'</a>'+data.text.slice(url.indices[1]);
 			}
 			message.text = data.text;
-//			if (log.length > 20)
-//			log.shift();
-//			log.push(message);
+			if (log.length > 20) log.shift();
+			log.push(message);
 			socket.broadcast(message);
 		}
 	}
