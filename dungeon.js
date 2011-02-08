10e4b20ddd037ea9271e14c21f120e4251e82cb8
arkie <email@joncaldwell.com>
2011-02-07 18:04:39 -0800

    added server side persistence

--- dungeon.js
+++ dungeon.js
@@ -1,152 +1,88 @@
-KEYS = {65:'left', 68:'right', 87:'up', 83: 'down', 16:'attack'}
+keys = {65:'left', 68:'right', 87:'up', 83: 'down', 16:'attack'}
 
+if (typeof(exports)=='undefined') exports = {}
 
-world = {}
+exports.world = {}, list = [], types = {};
 
-function shape(){
-	ctx.beginPath();
-	ctx.fillStyle = arguments[0];
-	ctx.moveTo(arguments[1], arguments[2]);
-	for (var i = 3; i < arguments.length; i+=2)
-		ctx.lineTo(arguments[i], arguments[i+1]);
-	ctx.fill();
+types.hero = function(id, data){
+	this.type       = 'hero';
+	this.id         = id;
+	this.collisions = [];
+	this.speed      = 5;
+	this.left       = data.left   || false;
+	this.right      = data.right  || false;
+	this.up         = data.up     || false;
+	this.attack     = data.attack || false;
+
+	//Start position
+	this.x = data.x || 100;
+	this.y = data.y || 100; 
+	this.draw = function(){
+		ctx.save();
+		ctx.translate(this.x, this.y);
+		ctx.fillStyle = (this.collisions.length)?'#f00':'#000';
+		ctx.fillRect(-10, -10, 20, 20);
+		ctx.restore();
+	}
+	this.update = function(){
+		speed = this.speed;
+		if ((this.left||this.right)&&(this.up||this.down))
+			speed = .7 * speed;
+		if (this.left) this.x -= speed;
+		if (this.right) this.x += speed;
+		if (this.up) this.y -= speed;
+		if (this.down) this.y += speed;
+	}
+	this.bounds = function(){
+		return {left:this.x-10, top:this.y-10,
+			right:this.x+10, bottom:this.y+10}
+	}
 }
 
+exports.init = function(){
+	setInterval(exports.main, 33);
+}
 
-
-exports = {
-	player : function(id, data){
-		this.type       = 'player';
-		this.id         = id;
-		this.collisions = [];
-		this.speed      = 5;
-		this.left       = data.left   || false;
-		this.right      = data.right  || false;
-		this.up         = data.up     || false;
-		this.attack     = data.attack || false;
-
-		//Start position
-		this.x = data.x || 100;
-		this.y = data.y || 100; 
-		this.draw = function(){
-			ctx.save();
-			ctx.translate(this.x, this.y);
-			var color = (this.collisions.length)?'#f00':'#000';
-			shape(color, 10, 0, -10, 8, 0, 0, -10, -8);
-			ctx.restore();
-		}
-		this.update = function(){
-			speed = this.speed;
-			if ((this.left||this.right)&&(this.up||this.down))
-				speed = .7 * speed;
-			if (this.left) this.x -= speed;
-			if (this.right) this.x += speed;
-			if (this.up) this.y -= speed;
-			if (this.down) this.y += speed;
-		}
-		this.bounds = function(){
-			return {left:this.x-5, top:this.y-5,
-				right:this.x+5, bottom:this.y+5}
-		}
-	},
-
-	monster : function(id, data){
-		this.type       = 'monster';
-		this.id         = id;
-		this.x          = data.x; 
-		this.y          = data.y; 
-		this.collisions = [];
-		this.speed      = 2;
-		this.draw = function(){
-			ctx.save();
-			ctx.translate(this.x, this.y);
-			ctx.fillRect(-5, -5, 5, 5);
-			ctx.restore();
-		},
-		this.bounds = function(){
-			return {left:this.x-5, top:this.y-5,
-				right:this.x+5, bottom:this.y+5}
-		}
-		this.update = function(){
-			if (this.collisions.length == 0){
-				speed = this.speed;
-				if ((this.x < world['player'].x||this.x >
-					world['player'].x)&&(this.y <
-					world['player']||this.y>world['player'].y)){
-					speed = .7 * speed;	
-				}
-				if (this.x < world['player'].x){
-					this.x += speed;
-				}
-				if (this.x > world['player'].x){
-					this.x -= speed;
-				}
-				if (this.y < world['player'].y){
-					this.y += speed;
-				}
-				if (this.y > world['player'].y){
-					this.y -= speed;
-				}
-			}
-			else{
-				speed = 0;
-				this.collisions = [];
+exports.main = function (){
+	if (typeof(ctx)!='undefined')
+		ctx.clearRect(0, 0, cvs.width, cvs.height);
+	for (var i = 0; i<list.length; i++){
+		var o = exports.world[list[i]];
+		var r1 = o.bounds()
+		for (var ii = i+1; ii < list.length; ii++){
+			var oo = exports.world[list[ii]];
+			var r2 = oo.bounds()
+			if (r1.top < r2.bottom && r2.top < r1.bottom &&
+					r1.left < r2.right && r2.left < r1.right){
+				o.collisions.push(oo);
+				oo.collisions.push(o);
 			}
 		}
 	}
-
-}
-
-function collision_detection(obj){
-	var r1 = world[obj].bounds()
-	for (var oobj in world){
-		if (obj != oobj){
-			var r2 = world[oobj].bounds()
-			if (r1.top < r2.bottom && r2.top < r1.bottom && 
-				r1.left < r2.right && r2.left < r1.right) {
-					world[obj].collisions.push(world[oobj].id);
-			}
-		}
+	for (var o in exports.world){
+		o = exports.world[o];
+		o.collisions = [];
+		o.update();
+		if (typeof(ctx)!='undefined') o.draw();
 	}
 }
 
-function update_world(){
-	ctx.clearRect(0, 0, cvs.width, cvs.height);
-	for (var obj in world){
-		collision_detection(obj);
-		if (world[obj].update) world[obj].update();
-		if (world[obj].draw) world[obj].draw();
-	}
-}
-
-function draw_grid(){
-	for (x = 0; x < SCREEN_WIDTH; x+=64){
-		ctx.moveTo(x,0);
-		ctx.lineTo(x, SCREEN_HEIGHT);
-	}
-	for (y = 0; y < SCREEN_HEIGHT; y+=64){
-		ctx.moveTo(0,y);
-		ctx.lineTo(SCREEN_WIDTH, y);
-	}
-
-	ctx.strokeStyle = "#eee";
-	ctx.stroke();
-
-}
-
-function main(){
-	update_world();
-	draw_grid();
-}
-
-
-function receive(data){
+exports.receive = function(data){
 	for (var k in data){
-		if (k in world){
-			if (data[k] == 'delete') delete world[k];
-			else for (var v in data[k]) world[k][v] = data[k][v];
+		if (k in exports.world){
+			if (data[k] == 'delete'){
+				for (i in list)
+					if (list[i] == k){
+					   	list.splice(i, 1);
+						break;
+					}
+				delete exports.world[k];
+			}
+			else for (var v in data[k])
+				exports.world[k][v] = data[k][v];
 		} else if (data[k]['type']){
-			world[k] = new exports[data[k]['type']](k, data[k]);
+			exports.world[k] = new types[data[k]['type']](k, data[k]);
+			list.push(k);
 		}
 	}
 }
@@ -154,79 +90,33 @@ function receive(data){
 function send(id, data){
 	socket.send(data);
 	var env = {}; env[id] = data;
-	receive(env);
+	exports.receive(env);
 }
 
-function touchMove(e){
-	e.preventDefault();
-	//var touch = e.touches[0];
-	//ctx.save();
-	//ctx.translate(touch.pageX, touch.pageY);
-	//ctx.fillRect(-45, -45, 45, 45);
-	//ctx.restore();
-}
-
-function touchStart(e){
-	e.preventDefault();
-	var touch = e.touches[0];
-	action = {};
-	if (touch.pageX > SCREEN_WIDTH/2){
-		action = 'right';
-	}
-	if (touch.pageX < SCREEN_WIDTH/2){
-		action = 'left';
-	}
-	if (action){
-		state = e.type == 'touchstart';
-		if (world['player'][action] != state){
-			env = {}; env[action] = state;
-			send('player', env);
-		}
-	}
-}
-
+if (typeof(window)!='undefined')
 window.onload = function(){
-	TOUCHSTART = 'ontouchstart' in document.documentElement;
-
 	socket = new io.Socket(null, {port: 8080});
 	socket.on('message', function(data){
-		receive(data);
+		exports.receive(data);
 	});
 	socket.connect();
 
-	SCREEN_WIDTH  = document.body.clientWidth;
-	SCREEN_HEIGHT = document.body.clientHeight;
-
 	cvs = document.getElementById('canvas');
-	cvs.width  = SCREEN_WIDTH; 	
-	cvs.height = SCREEN_HEIGHT;
+	cvs.width  = document.width;
+	cvs.height = document.height;
 	ctx = cvs.getContext('2d');
 
-	send('player', {type:'player'});
-	enemies = {}
-	for (var i = 0; i < 5; i++)
-		send('monster-'+i, {type:'monster',
-		x:Math.floor(Math.random()*SCREEN_WIDTH),
-		y:Math.floor(Math.random()*SCREEN_HEIGHT)});
-	if (TOUCHSTART == false){
-		window.onkeyup = window.onkeydown = function(e){
-			action = {}
-			action = KEYS[e.keyCode];
-			if (action){
-				state = e.type == 'keydown';
-				if (world['player'][action] != state){
-					env = {}; env[action] = state;
-					send('player', env);
-				}
+	send('@', {type:'hero'});
+	window.onkeyup = window.onkeydown = function(e){
+		action = {}
+		action = keys[e.keyCode];
+		if (action){
+			state = e.type == 'keydown';
+			if (exports.world['@'][action] != state){
+				env = {}; env[action] = state;
+				send('@', env);
 			}
 		}
 	}
-	else {
-		canvas.addEventListener('touchmove', touchMove, false);	
-		canvas.addEventListener('touchstart', touchStart, false);	
-	}
-	setInterval(main, 33);
-	setInterval(function(){
-		send('player', world['player']);
-	}, 1000);
+	exports.init();
 }
--- node.js
+++ node.js
@@ -4,42 +4,43 @@ var http = require('http'),
 	fs   = require('fs');
 	io   = require('./socket.io'),
 
-server = http.createServer(
+mime = {'html':'text/html', 'js':'text/javascript'}
+
+var server = http.createServer(
 	function(req, res){
-		if (req.url == '/'){
-			res.writeHead(200, {'Content-Type':'text/html'});
-			fs.readFile('index.html', function(err, data){
+		file = req.url.slice(1)||'index.html';
+		fs.readFile(file, function(e, data){
+			if (e){
+				res.writeHead(500);
+				res.end('404: Not Found');
+				console.log('Error reading [' + file + ']');
+			} else {
+				res.writeHead(200, {'Content-Type':
+					mime[file.slice(file.lastIndexOf('.')+1)]});
 				res.end(data);
-			});
-		} else if (req.url == '/dungeon.js'){
-			res.writeHead(200, {'Content-Type':'text/javascript'});
-			fs.readFile('dungeon.js', function(err, data){
-				res.end(data);
-			});
-		} else {
-			res.writeHead(204);
-			res.end();
-		}
+			}
+		});
 	});
 
 server.listen(8080, '0.0.0.0');
 
-var io = io.listen(server); //, buffer = [];
+var io = io.listen(server);
+
+var game = require(process.argv[2]);
+game.init();
 
 io.on('connection', function(client){
-	client.send();
+	client.send(game.world);
 
 	client.on('message', function(message){
-//		var msg = { message: [client.sessionId, message] };
-//		buffer.push(msg);
-//		if (buffer.length > 15) buffer.shift();
 		var env = {}; env[client.sessionId] = message;
+		game.receive(env);
 		client.broadcast(env);
-//		console.log(msg);
 	});
 
 	client.on('disconnect', function(){
 		var env = {}; env[client.sessionId] = 'delete';
+		game.receive(env);
 		client.broadcast(env);
 	});
 });
