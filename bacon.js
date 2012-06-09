a12af2d23df85eec36fd6bb44e9e8dc25dc9c4d5
arkie <email@joncaldwell.com>
2012-06-09 14:49:48 -0700

    back and forth :)

--- bacon.js
+++ bacon.js
@@ -14,10 +14,12 @@ var sign = function(){
 			.update(time.toString())).digest('hex');
 }
 
-var returns = {moviegenres: 'genres', significantmovies: 'movies'};
+var returns = {
+	'descriptor/moviegenres': 'genres',
+	'descriptor/significantmovies': 'movies'};
 
 var rovio = function(resource, params, callback) {
-	var path = '/data/v1/descriptor/' + resource +
+	var path = '/data/v1/' + resource +
 		'?country=US&language=en&format=json&apikey=' +
 		conf.key + '&sig=' + sign();
 	for (var k in params) {
@@ -33,30 +35,81 @@ var rovio = function(resource, params, callback) {
 				body += chunk;
 			});
 			res.on('end', function(chunk){
-				callback(JSON.parse(body)[returns[resource]]);
+				callback(JSON.parse(body)[
+					returns[resource]||resource.split('/')[1]]);
 			});
 		});
 }
 
-var start = function(req) {
-	rovio('moviegenres', {include:'subgenres'}, function(genres) {
-		rovio('significantmovies', {
-			genreids:genres[0].id.replace(/ /g, '+')
-		}, function(movies) {
-			var from = movies[0];
-			var to = movies[1];
-			req.end('Go from <img src="' + from['thumbnail'] +
-				'"> to <img src="' + to['thumbnail'] + '">.');
-		});
+var id = function(target) {
+	return target.replace(/ /g, '+');
+}
+
+var movie = function(target) {
+	return target['title'] + ' (' + target['releaseYear'] + ')';
+}
+
+var actor = function(target) {
+	return target['name'] + ' (' + target['role'] + ')';
+}
+
+var actors = function(req, source) {
+	rovio('movie/cast', {movieId: id(source)}, function(cast) {
+		for (k in cast) {
+			var member = cast[k];
+			console.log(member);
+			if (member.thumbnail) {
+			req.write('<a href="/actor/' + id(member.id) + '"><img src="' +
+				member.thumbnail + '" title="' + actor(member) + '"></a>');
+			}
+		}
+		req.end();
 	});
 }
 
+var movies = function(req, source) {
+	rovio('name/filmography', {amgmovieid: id(source)}, function(films) {
+		for (k in films) {
+			var feature = films[k];
+			console.log(feature);
+			req.write('<div><a href="/movie/' + id(feature.id) + '">' +
+			   feature.title + ' (' + feature.year + ')</a></div>');
+		}
+		req.end();
+	});
+}
+
+var start = function(req) {
+	rovio('descriptor/moviegenres', {include:'subgenres'}, function(genres) {
+		rovio('descriptor/significantmovies', {
+			genreids:id(genres[0].id)
+		}, function(movies) {
+			var from = movies[0];
+			console.log(from);
+			var to = movies[1];
+			req.write('Go from <img src="' + from['thumbnail'] +
+				'" title="' + movie(from) + '"> to <img src="' +
+				to.thumbnail + '" title="' + movie(to) + '">.<hr>');
+			actors(req, from.id);
+		});
+	});
+};
+
 http.createServer(
 	function(res, req) {
-		switch (res.url) {
-			case '/':
-			req.writeHead(200, {'Content-Type': 'text/html'});
-			start(req);
-			break;
+		var parts = res.url.split('/');
+		switch (parts[1]) {
+			case '':
+				req.writeHead(200, {'Content-Type': 'text/html'});
+				start(req);
+				break;
+			case 'actor':
+				req.writeHead(200, {'Content-Type': 'text/html'});
+				movies(req, parts[2]);
+				break;
+			case 'movie':
+				req.writeHead(200, {'Content-Type': 'text/html'});
+				actors(req, parts[2]);
+				break;
 		}
 	}).listen(8080, '0.0.0.0');
