d10e94854306c43be9f2679b990dee27942fc5ef
Jon Caldwell <char7f@gmail.com>
2016-06-15 21:18:03 -0700

    Use a desktop-ish styling, media-select out 3ds

--- pages/pokelist.js
+++ pages/pokelist.js
@@ -61,7 +61,7 @@ loop: while (true) {
       a.className = 'image ' + p.identifier;
       a.href = 'http://veekun.com/dex/pokemon/' + p.name.replace(' ', '');
       a.title = p.id + ': ' + p.name;
-      data.dataset.capture = capture[p.id];
+      data.dataset.capture = !!capture[p.id];
       data.appendChild(a);
       row.appendChild(data);
       if (count % 6 == 0) {
@@ -78,107 +78,113 @@ function poke(id) {
   var span = document.createElement('span');
   span.className = 'image ' + p.identifier;
   span.title = p.id + ': ' + p.name;
+  span.dataset.capture = capture[id];
   return span
 }
 var info = document.getElementById('info');
-window.scrollTo(40, 300);
 function showInfo(id) {
   info.style.display = 'block';
-  var p = POKEDEX[id];
-  if (!p) {
+  var prime = POKEDEX[id];
+  if (!prime) {
     return;
   }
   while (info.firstChild) {
     info.removeChild(info.firstChild);
   }
-  var div = document.createElement('div');
-  div.className = 'name';
-  var id = document.createElement('span');
-  id.className = 'id';
-  id.textContent = p.id;
-  div.appendChild(id);
-  div.appendChild(document.createTextNode(p.name));
-  var image = document.createElement('span');
-  image.className = 'preview image ' + p.identifier;
-  div.appendChild(image);
-  info.appendChild(div);
-  //info.appendChild(document.createTextNode(JSON.stringify(p, null, 4)));
-  if (p.chain[0] == p.id && (
-        p.chain.length > 1 ||
-        p.egg.indexOf('Undiscovered') == -1)) {
-    var breed = document.createElement('div');
-    breed.className = 'breed';
-    breed.appendChild(document.createTextNode('Breed'));
-    p.chain.forEach(function(i) {
-      if (POKEDEX[i].egg.indexOf('Undiscovered') == -1) {
-        breed.appendChild(poke(i));
+  prime.chain.forEach(function(id) {
+    var p = POKEDEX[id];
+    var pokediv = document.createElement('div');
+    var div = document.createElement('div');
+    div.className = 'name';
+    var id = document.createElement('span');
+    id.className = 'id';
+    id.textContent = p.id;
+    div.appendChild(id);
+    div.appendChild(document.createTextNode(p.name));
+    var image = document.createElement('span');
+    image.className = 'preview image ' + p.identifier;
+    image.dataset.capture = !!capture[p.id];
+    div.appendChild(image);
+    pokediv.appendChild(div);
+    //info.appendChild(document.createTextNode(JSON.stringify(p, null, 4)));
+    if (p.chain[0] == p.id && (
+          p.chain.length > 1 ||
+          p.egg.indexOf('Undiscovered') == -1)) {
+      var breed = document.createElement('div');
+      breed.className = 'breed';
+      breed.appendChild(document.createTextNode('Breed'));
+      p.chain.forEach(function(i) {
+        if (POKEDEX[i].egg.indexOf('Undiscovered') == -1) {
+          breed.appendChild(poke(i));
+        }
+      });
+      if (p.baby_item) {
+        breed.appendChild(document.createTextNode('holding ' + p.baby_item));
       }
-    });
-    if (p.baby_item) {
-      breed.appendChild(document.createTextNode('holding ' + p.baby_item));
+      pokediv.appendChild(breed);
     }
-    info.appendChild(breed);
-  }
-  if (p.precursor) {
-    var pre = document.createElement('div');
-    pre.className = 'breed';
-    pre.appendChild(document.createTextNode(p.trade ? 'Trade' : 'Evolve'));
-    pre.appendChild(poke(p.precursor));
-    var text = [];
-    if (p.level) {
-      text.push('at Lvl', p.level);
-    }
-    if (p.item) {
-      text.push('using', p.item);
-    }
-    if (p.held) {
-      text.push('holding', p.held);
-    }
-    if (p.happy) {
-      text.push('with', p.happy, 'Happiness');
-    }
-    if (p.check) {
-      text.push('*');
-    }
-    if (text.length) {
-      pre.appendChild(document.createTextNode(text.join(' ')));
-    }
-    info.appendChild(pre);
-  }
-  if (p.catch) {
-    var locales = {};
-    p.catch.forEach(function(c) {
-      if (!locales[c.location]) {
-        locales[c.location] = {};
+    if (p.precursor) {
+      var pre = document.createElement('div');
+      pre.className = 'breed';
+      pre.appendChild(document.createTextNode(p.trade ? 'Trade' : 'Evolve'));
+      pre.appendChild(poke(p.precursor));
+      var text = [];
+      if (p.level) {
+        text.push('at Lvl', p.level);
       }
-      var l = locales[c.location];
-      if (!l[c.method]) {
-        l[c.method] = {};
+      if (p.item) {
+        text.push('using', p.item);
       }
-      l[c.method][c.game] = true;
-    });
-    for (var k in locales) {
-      var locale = document.createElement('div');
-      locale.className = 'catch';
-      locale.appendChild(document.createTextNode(k));
-      var ul = document.createElement('ul');
-      for (var k2 in locales[k]) {
-        var li = document.createElement('li');
-        li.appendChild(document.createTextNode(k2 + ' (' +
-          Object.keys(locales[k][k2]).join(', ') + ')'));
-        ul.appendChild(li);
+      if (p.held) {
+        text.push('holding', p.held);
       }
-      locale.appendChild(ul);
-      info.appendChild(locale);
+      if (p.happy) {
+        text.push('with', p.happy, 'Happiness');
+      }
+      if (p.check) {
+        text.push('*');
+      }
+      if (text.length) {
+        pre.appendChild(document.createTextNode(text.join(' ')));
+      }
+      pokediv.appendChild(pre);
     }
-  }
+    info.appendChild(pokediv);
+    if (prime.id != p.id) {
+      pokediv.className = 'other';
+    } else {
+      pokediv.className = 'selected';
+      if (p.catch) {
+        var locales = {};
+        p.catch.forEach(function(c) {
+          if (!locales[c.location]) {
+            locales[c.location] = {};
+          }
+          var l = locales[c.location];
+          if (!l[c.method]) {
+            l[c.method] = {};
+          }
+          l[c.method][c.game] = true;
+        });
+        for (var k in locales) {
+          var locale = document.createElement('div');
+          locale.className = 'catch';
+          locale.appendChild(document.createTextNode(k));
+          var ul = document.createElement('ul');
+          for (var k2 in locales[k]) {
+            var li = document.createElement('li');
+            li.appendChild(document.createTextNode(k2 + ' (' +
+              Object.keys(locales[k][k2]).join(', ') + ')'));
+            ul.appendChild(li);
+          }
+          locale.appendChild(ul);
+          pokediv.appendChild(locale);
+        }
+      }
+      info.scrollTop = pokediv.offsetTop;
+    }
+  });
 }
-var reshow;
-window.addEventListener('scroll', function(e) {
-  clearTimeout(reshow);
-  info.style.display = 'none';
-  reshow = setTimeout(showInfo, 100);
-});
 var pokelist = boxes.getElementsByTagName('a');
 var query = document.getElementById('query');
 query.addEventListener('input', function(e) {
@@ -194,12 +200,6 @@ query.addEventListener('input', function(e) {
     query.className = 'err';
   }
 });
-var qrcode = document.getElementById("qrcode");
-qrcode.addEventListener('click', function(e) {
-  e.preventDefault();
-  rehash();
-  window.open('http://qrf.in/?url=' + encodeURIComponent(window.location.href), '_blank');
-});
 for (var i = 0; i < pokelist.length; i++) {
   var p = pokelist[i];
   p.addEventListener('click', function(e){
@@ -209,13 +209,23 @@ for (var i = 0; i < pokelist.length; i++) {
     e.preventDefault();
     var id = e.target.id;
     capture[id] = !capture[id];
-    e.target.parentNode.dataset.capture = capture[id];
+    e.target.dataset.capture = capture[id];
     rehash();
+    showInfo(id);
   });
   p.addEventListener('mouseover', function(e){
     showInfo(e.target.id);
   });
   p.addEventListener('focusin', function(e){
     showInfo(e.target.id);
+  });
+}
+if (window.matchMedia('(device-width: 320px)').matches) {
+  window.scrollTo(40, 300);
+  var reshow;
+  window.addEventListener('scroll', function(e) {
+    clearTimeout(reshow);
+    info.style.display = 'none';
+    reshow = setTimeout(showInfo, 100);
   });
 }
