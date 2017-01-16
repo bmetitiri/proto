3c6278f947c15b2b0d877819b28f57bfe566245e
Jon Caldwell <char7f@gmail.com>
2017-01-15 19:24:46 -0800

    Add close button in bookmarklet

--- static/bookmark.js
+++ static/bookmark.js
@@ -3,21 +3,37 @@
   var s = document.body.lastElementChild;
   document.body.removeChild(s);
   let query = window.location.hostname.split('.').slice(-2)[0];
+  let container = document.createElement('div');
+  container.style.position = 'fixed';
+  container.style.bottom = 0;
+  container.style.right = '25px';
+  container.style.top = 0;
+  container.style.width = '270px';
+  container.style.zIndex = 1 << 24;
   let frame = document.createElement('iframe');
   frame.src = `${new URL(s.src).origin}/?q=${query}`;
-  frame.style.background = '#eee';
   frame.style.border = 0;
   frame.style.boxShadow = '0 0 5px 0 #999';
-  frame.style.height = '100vh';
-  frame.style.position = 'fixed';
-  frame.style.right = '25px';
-  frame.style.top = 0;
-  frame.style.zIndex = 1 << 24;
-  document.body.appendChild(frame);
-  document.body.addEventListener('keyup', function handler(e) {
+  frame.style.height = '100%';
+  frame.style.width = '100%';
+  container.appendChild(frame);
+  let close = document.createElement('input');
+  close.type = 'button';
+  close.value = '❌';
+  close.style.position = 'absolute';
+  close.style.top = '15px';
+  close.style.right = 0;
+  close.addEventListener('click', exit);
+  container.appendChild(close);
+  document.body.appendChild(container);
+  document.body.addEventListener('keyup', keyup);
+  function keyup(e) {
     if (e.keyCode == 27) {
-      document.body.removeEventListener('keyup', handler);
-      document.body.removeChild(frame);
+      exit();
     }
-  });
+  }
+  function exit() {
+    document.body.removeEventListener('keyup', keyup);
+    document.body.removeChild(container);
+  }
 })();
