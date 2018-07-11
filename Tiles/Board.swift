971b8a3469f5c4317c0b00f7ec63e026f1e5fb5e
Jon Caldwell <char7f@gmail.com>
2018-07-10 23:51:51 -0700

    Now with falling blocks

---
+++ Tiles/Board.swift
@@ -0,0 +1,64 @@
+import SpriteKit
+
+class Board: SKNode {
+    let width = 10
+    let height = 10
+    var board = [Tile?](repeating: nil, count: 10 * 10) // width * height
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        if let data = userData, let board = data["board"] as? [Tile] {
+            self.board = board
+        }
+        position = CGPoint(
+            x: (Double(-width) + 0.5) * Double(Tile.sideLength) / 2,
+            y: (Double(-height) + 0.5) * Double(Tile.sideLength) / 2)
+        tick()
+    }
+    
+    func get(x: Int, y: Int) -> Tile? {
+        let i = y * height + x
+        return 0 <= i && i < board.count ? board[i] : nil
+    }
+    
+    func set(x: Int, y: Int, tile: Tile?) {
+        let i = y * height + x
+        if 0 <= i && i < board.count {
+            board[y * height + x] = tile
+        }
+    }
+    
+    func tick() {
+        isUserInteractionEnabled = false
+        let delay = fall()
+        if delay > 0 {
+            run(SKAction.wait(forDuration: delay), completion:tick)
+        }
+        isUserInteractionEnabled = true
+    }
+    
+    func fall() -> TimeInterval {
+        var delay = 0.0
+        for x in 0 ..< width {
+            for y in 0 ..< height {
+                if let tile = get(x: x, y: y) {
+                    if tile.type == .empty {print(tile)}
+                } else {
+                    if y == height - 1 {
+                        let sprite = Tile(type:TileType.random, x: x, y: y+1)
+                        set(x:x, y:y, tile: sprite)
+                        addChild(sprite)
+                        sprite.move(x:x, y:y)
+                        delay = Tile.fallTime
+                    } else if let above = get(x: x, y: y + 1) {
+                        set(x:x, y:y + 1, tile: nil)
+                        set(x:x, y:y, tile: above)
+                        above.move(x:x, y:y)
+                        delay = Tile.fallTime
+                    }
+                }
+            }
+        }
+        return delay
+    }
+}
---
+++ Tiles/Tile.swift
@@ -0,0 +1,51 @@
+import SpriteKit
+
+enum TileType {
+    case empty
+    case red
+    case blue
+    case green
+    
+    var color: UIColor {
+        switch self {
+        case .empty: return UIColor.clear
+        case .red: return UIColor.red
+        case .blue: return UIColor.blue
+        case .green: return UIColor.green
+        }
+    }
+    
+    static var random: TileType {
+        let colors = [TileType.red, TileType.blue, TileType.green]
+        return colors[Int(arc4random_uniform(UInt32(colors.count)))]
+    }
+}
+
+class Tile : SKSpriteNode {
+    static let sideLength = 20
+    static let fallTime = 0.1
+    let type: TileType
+    var x: Int, y: Int
+    
+    required init?(coder: NSCoder) {
+        fatalError("How did you get here?!")
+    }
+    
+    var point: CGPoint {
+        return CGPoint(x: x * Tile.sideLength, y: y * Tile.sideLength)
+    }
+    
+    init(type: TileType, x: Int, y: Int) {
+        self.x = x
+        self.y = y
+        self.type = type
+        super.init(texture: nil, color: type.color, size: CGSize(width: Tile.sideLength, height: Tile.sideLength))
+        position = point
+    }
+    
+    func move(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+        run(SKAction.move(to: point, duration: Tile.fallTime))
+    }
+}
