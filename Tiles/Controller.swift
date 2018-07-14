100c8fc02928f619da9085d84ecce9a600c219ff
Jon Caldwell <char7f@gmail.com>
2018-07-14 12:40:32 -0700

    Such easy constraints :)

--- Tiles/Controller.swift
+++ Tiles/Controller.swift
@@ -5,12 +5,19 @@ class Controller: UIViewController {
     let board = Board()
     override func viewDidLoad() {
         super.viewDidLoad()
-        let scene = SKScene(size: CGSize(width: 200, height: 200))
+        let scene = SKScene(size: CGSize(width: 200, height: 300))
         scene.scaleMode = .aspectFit
         scene.addChild(board)
         let skView = SKView(frame: view.bounds)
+        skView.backgroundColor = UIColor.black
         skView.ignoresSiblingOrder = true
-        skView.presentScene(scene)
         view.addSubview(skView)
+        NSLayoutConstraint.activate([
+            skView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            skView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            skView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+            skView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+        ])
+        skView.presentScene(scene)
     }
 }
